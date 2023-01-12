baf431714522dd49c34ccc9cd86569d365f03655
njeutsa <hermann.njeutsa@korv-consulting.fr>
2023-01-12 15:43:20 +0100

    [feat-stepper]: next and preview functions are ok

--- components/auth/signup/step-one.js
+++ components/auth/signup/step-one.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react'
 
 function StepOneSignup() {
 
+    const [active, setActive] = useState(1)
     const [filled, setFilled] = useState(33)
     const [added, setAdded] = useState(filled - 30)
 
@@ -21,60 +22,88 @@ function StepOneSignup() {
     };
 
 
-    const next = (step, filled) => {
+    const next = (step) => {
         switch (step) {
             case 1:
                 setFilled(210)
                 setAdded(210- 30)
-
+                setActive(2)
+                colorLineStepper(step)
                 break;
             case 2:
                 setFilled(390)
                 setAdded(390- 30)
-
+                setActive(3)
+                colorLineStepper(step)
                 break;
             case 3:
                 setFilled(570)
                 setAdded(570- 30)
+                setActive(4)
+                colorLineStepper(step)
 
                 break;
             default:
-                setFilled(filled)
-                setAdded(filled - 30)
+                setFilled(33)
+                setAdded(33 - 30)
+                setActive(1)
+
 
         }
 
 
     };
 
-    const preview = (step, filled) => {
+    const preview = (step) => {
         switch (step) {
 
             case 2:
                 setFilled(33)
                 setAdded(33 - 30)
+                setActive(1)
+                removeColorLineStepper(step)
 
                 break;
             case 3:
                 setFilled(210)
                 setAdded(210 - 30)
+                setActive(2)
+                removeColorLineStepper(step)
 
                 break;
             case 4:
                 setFilled(390)
                 setAdded(390 - 30)
+                setActive(3)
+                removeColorLineStepper(step)
 
                 break;
             default:
-                setFilled(filled)
-                setAdded(filled - 30)
+                setFilled(33)
+                setAdded(33 - 30)
+                setActive(1)
+
 
         }
 
 
     };
 
+    const colorLineStepper = (step) => {
+        for(let i=1;i<=step;i++){
+            let element = document.getElementById(i)
+            element.style.border = "2px solid #03DAC5"
+           // console.log(element)
 
+        }
+
+    }
+
+    const removeColorLineStepper = (step) => {
+       let element = document.getElementById(step-1)
+       element.style.border = "2px solid black"
+        colorLineStepper(step-2)
+    }
 
 
 
@@ -98,7 +127,7 @@ function StepOneSignup() {
 
                                     <div className={styles.subtitleleft}>
                                         <center>
-                                            <div className={styles.txtleleft} onClick={e=>{next(5, filled)}} >Particulier</div>
+                                            <div className={styles.txtleleft} onClick={e=>{preview(3)}} >Particulier</div>
                                         </center>
                                     </div>
                                 </div>
@@ -112,7 +141,7 @@ function StepOneSignup() {
 
                                     <div className={styles.subtitleright}>
                                         <center>
-                                            <div className={styles.txtright} onClick={e=>{next(2, filled)}} >Professionnel</div>
+                                            <div className={styles.txtright} onClick={e=>{preview(4)}} >Professionnel</div>
                                         </center>
 
                                     </div>
@@ -134,13 +163,13 @@ function StepOneSignup() {
                     <Row>
                         <Col sm={4} ></Col>
                         <Col sm={4} className={styles.box_number}>
-                            <div className={styles.number} >1 </div>
-                            <div className={styles.linear} ></div>
-                            <div className={styles.number} >2 </div>
-                            <div className={styles.linear} ></div>
-                            <div className={styles.number} >3</div>
-                            <div className={styles.linear} ></div>
-                            <div className={styles.number} >4 </div>
+                            <div  className={active==1? styles.number_active : styles.number} >1 </div>
+                            <div id="1" className={styles.linear} ></div>
+                            <div className={active==2? styles.number_active : styles.number} >2 </div>
+                            <div id="2" className={styles.linear} ></div>
+                            <div className={active==3? styles.number_active : styles.number} >3</div>
+                            <div id="3" className={styles.linear} ></div>
+                            <div className={active==4? styles.number_active : styles.number} >4 </div>
 
                         </Col>
                         <Col sm={4}></Col>
@@ -162,29 +191,26 @@ function StepOneSignup() {
                                 <div style={{
                                     height: "100%",
                                     width: `${filled}px`,
-                                    backgroundColor: "#2DB6FE",
+                                    backgroundColor: "#03DAC5",
                                     transition: "width 0.1s",
                                     borderRadius: "500px 500px 500px 500px"
                                 }}>
                                 </div>
                                 <div style={{
-                                    borderTop: "12px solid #fff",
-                                    borderBottom: "15px solid #fff",
-                                    borderLeft: "15px solid #fff",
-                                    borderRight: "12px solid #fff",
-                                    backgroundColor: "#fff",
+                                    border: "2px solid #2DB6FE",
+                                    backgroundColor: "#DEF4FF",
                                     transition: "width 0.1s",
                                     borderRadius: "100%",
                                     marginLeft: "-6%",
                                     width: "40px",
+                                    height: "27px",
                                     zIndex: "1",
                                     marginTop: "-27px",
                                     transform: `translate(${filled}px, 0px)`
 
                                 }}>
 
-                                </div>
-
+                                </div>     
 
                             </div>
 
