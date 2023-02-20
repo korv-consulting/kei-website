1e137878a0e17da33804dd17b7cd87b6a6a86cb3
njeutsa <hermann.njeutsa@korv-consulting.fr>
2023-02-20 14:57:09 +0100

    [fix-login page]:reponsive is ok

--- components/auth/signin/sign-in.js
+++ components/auth/signin/sign-in.js
@@ -1,6 +1,6 @@
 import { Button, ButtonGroup, Card, Col, Container, Image, Row } from "react-bootstrap"
 import styles from '../../../styles/Sign-in.module.css'
-import style from '../../../styles/Floating-label.module.css'
+import style from '../../../styles/Floating-label-improved.module.css'
 
 import React, { useState, useEffect } from 'react';
 import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
@@ -24,14 +24,14 @@ function SignIn() {
 
     };
 
-   
+
     const handleClickPwd = (state) => {
         setShowPwd(!state)
         var x = document.getElementById("pwd");
         if (x.type === "password") {
-          x.type = "text";
+            x.type = "text";
         } else {
-          x.type = "password";
+            x.type = "password";
         }
     };
 
@@ -39,82 +39,74 @@ function SignIn() {
 
     return (
         <form className={styles.form} id="form" method="post" noValidate onSubmit={onSubmit}>
-              
-                <div className="text-center">
-                    <p  className={`mt-5  ${styles.blue}`} >Connexion</p>
-                    <p className={styles.remember}>Entrez vos informations et commencez la journée avec nous</p>
-                </div>
 
-                <Row className={styles.row_center}  >
-                    <Col md={2}></Col>
-                    <Col xs={12} md={8}  >
-                        <h5 className={`text-center text-danger  ${styles.hide}`} id="hide_pwd" >
-                            Les mots de passe saisis ne sont pas identiques
-                        </h5><b /><br />
-                        <div className={styles.box_pwd}>
+            <p className="text-center">
+                <p className={`mt-5  ${styles.blue}`} >Connexion</p>
+                <p className={styles.subtitle}>Entrez vos informations et commencez la journée avec nous</p>
+            </p>
 
-                            <input type="email" id='mail' placeholder='example@gmail.com' className={`form-control ${style.input_text}`} required name="email"
-                                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                                title="L'E-Mail doit contenir au moins le caractere @"
-                            />
-                            <label htmlFor='mail' id='umail' className={style.label}>E-MAIL</label>
-                            <span id="email" className={`invalid-feedback ${styles.errorMsg}`} >
-                                Veuillez saisir votre E-MAIL
-                            </span>
+            <h5 className={`text-center text-danger  ${styles.hide}`} id="hide_pwd" >
+                Les mots de passe saisis ne sont pas identiques
+            </h5><b /><br />
 
-                            <Image fluid src="./signin/envelop.png" className={styles.envelop_icon} alt="Sky Crappers" />
+            <div className={`input-group  mb-4 ${styles.groupA}`} >
+                <input type="email" id='mail' placeholder='example@gmail.com' className={`form-control ${style.input_text}`} required name="email"
+                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                    title="L'E-Mail doit contenir au moins le caractere @"
+                />
+                <span  className={`input-group-text bg-white border-left-0 ${style.span}`} id="basic-addon1">
+                    <Image fluid src="./signin/envelop.png" alt="Sky Crappers" />
+                </span>
+                <label htmlFor='mail' id='umail'  className={`ms-4 ${style.label}`}>E-MAIL </label>
 
-                        </div><br />
-                        <div className={styles.box_pwd}>
-                            <input type="password" id='pwd' placeholder='........' className={`form-control ${style.input_text}`} required name="pwd"
-                                title="mot de passe"
-                            />
-                            <label htmlFor='pwd' id="label_pwd" className={style.label}>Mot de passe</label>
-                            <Image fluid src="./signin/padlock.png" onClick={() => handleClickPwd(showPwd)} className={styles.envelop_icon} alt="Sky Crappers" />
+            </div>
+            <span id="email" className={`invalid-feedback ${styles.errorMsg}`} >
+                Veuillez saisir votre E-MAIL
+            </span>
 
-                            <span className={`invalid-feedback ${styles.errorMsg}`} >
-                                Veuillez entrer votre mot de passe
-                            </span>
-                        </div><br />
-                        <div className='d-flex justify-content-between'> 
-                            <span className={styles.box_left}>
-                                <input type="checkbox" id='' className={` ${styles.checkbox}`} required name="checkbox"
-                                    title="Cochez la case"
-                                />
-                                <span className={styles.remember}>Se souvenir de moi</span>
-                            </span>
-                            <span className={styles.forgot_pwd}>
-                                Mot de passe oublié?
-                            </span>
-                        </div>
-                        <Button type="submit" className={`my-4 ${styles.btn}`} >
-                            <span className={(loadSpiner == true && submitted == true ) ? styles.disable:styles.spinner} > CONNEXION </span>
-                            <div className={loadSpiner == true && submitted == true ? `spinner-border mr-12 ${styles.spinner}` : `${styles.disable}`} role="status">
-                                <span className={`visually-hidden`} >Loading...</span>
-                            </div>
-                         </Button>
-                    </Col>
-                    <Col  md={2}></Col>
+            <div className={`input-group mb-4 ${styles.groupB}`}>  
+                <input type="password" id='pwd' placeholder='........' className={`form-control   ${style.input_text}`} required name="pwd"
+                    title="mot de passe"
+                />
+                <span  className={`input-group-text bg-white border-left-0  ${style.span}`} id="basic-addon3">
+                <Image fluid src="./signin/padlock.png"   onClick={() => handleClickPwd(showPwd)}  alt="Sky Crappers" />
+                </span>
+                <label htmlFor='pwd' id='upwd'  className={` ${style.label}`}>Mot de passe</label>
 
-                </Row>
-                <Row>
-                    <Col ></Col>
-                    <Col md={4} className={styles.box_bottom} >
-                 
-                        <div className={styles.txt} >
-                            <span className={styles.txt_blue} >Vous avez deja un compte ? </span>
-                            <span className={styles.txt_green}>
-                                Connexion
-                            </span>
-                        </div>
+            </div>
+            <span className={`invalid-feedback ${styles.errorMsg}`} >
+                Veuillez entrer votre mot de passe
+            </span>
+            <br />
+            <Row className={styles.box_remember} >
+                <Col xs={12} md={6} className={styles.box_left}>
+                    <input type="checkbox" id='' className={` ${styles.checkbox}`} required name="checkbox"
+                        title="Cochez la case"
+                    />
+                    <span  className={styles.remember}>Se souvenir de moi</span>
+                </Col>
+                <Col md={6} className={styles.forgot_pwd}>
+                    Mot de passe oublié?
+                </Col>
+            </Row>
+            <Button type="submit" className={`my-4 ${styles.btn}`} >
+                <h4 className={(loadSpiner == true && submitted == true) ? styles.disable : styles.btn_txt} > CONNEXION </h4>
+                <span className={loadSpiner == true && submitted == true ? `spinner-border mr-12 ${styles.spinner}` : `${styles.disable}`} role="status">
+                    <h3 className={`visually-hidden`} >Loading...</h3>
+                </span>
+            </Button>
+
+            <Row className={styles.txt} >
+                <Col xd={12} md={7} className={styles.txt_blue} >Vous avez deja un compte ? </Col>
+                <Col md={5} className={styles.txt_green}>
+                    Connexion
+                </Col>
+            </Row>
 
 
-                    </Col>
-                    <Col></Col>
 
-                </Row>
 
-            
+
 
         </form>
 
--- components/auth/signin/signin-component.js
+++ components/auth/signin/signin-component.js
@@ -1,6 +1,5 @@
 import { Button, ButtonGroup,Card, Col, Container, Image, Row } from "react-bootstrap"
 import styles from '../../../styles/Signin-Component.module.css'
-import style from '../../../styles/Floating-label.module.css'
 import React, { useState, useEffect } from 'react';
 import SignIn from './sign-in'
 
@@ -8,37 +7,38 @@ function SigninComponent({next,preview}) {
 
     const onSubmit = (event) => {
         event.preventDefault();
-        alert(`Hey, onSubmiting...`);
+        alert(`Hey, onSubmiting...`); 
 
     };
 
 
     return(
-    <Container fluid className={styles.main}>
-        <Image fluid src="/skyscraper.png" className={styles.background} alt="Sky Crappers" />
-        <Row className={styles.global_row}>
+    <Container fluid className={`p-0  ${styles.main}`} >
+        <section  className={`${styles.section}`}>
+            <Row   >
 
-            <Col></Col>
-            <Col  xs={12} md={5} >   
-                <div>
-                 <Image fluid src="signup/KEI-without-bg.png" className={styles.picture} alt="Sky Crappers" />
-                </div>
-                <Card > 
-                     <SignIn 
-                          next={next}
-                          preview={preview}
-                     />
-                  
-                </Card>
-            </Col> 
-            <Col  className={` ${styles.box_btn}`} > 
-                <p>
-                        <Button className={styles.btn_welcome_back}>Bon Retour ! </Button>
-                        <Image fluid src="signin/icon-awesome-smile-beam.png" className='mx-4' alt="smile" />
-                </p>
-            </Col>
+                <Col></Col>
+                <Col  md={12} lg={5} className="mb-5" >   
+                
+                        <Image fluid src="signup/KEI-without-bg.png" className={styles.picture} alt="Sky Crappers" />
+                    
+                    <Card className="px-2 m-auto" > 
+                        <SignIn 
+                            next={next}
+                            preview={preview}
+                        />
+                    
+                    </Card>
+                </Col> 
+                <Col  className={` ${styles.box_btn}`} > 
+                    <p>
+                            <Button className={styles.btn_welcome_back}>Bon Retour ! </Button>
+                            <Image fluid src="signin/icon-awesome-smile-beam.png" className='mx-4' alt="smile" />
+                    </p>
+                </Col>
 
-        </Row>
+            </Row>
+        </section>
         
     </Container>
     )
