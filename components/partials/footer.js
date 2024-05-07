3df30291b073c202e12e8ea5d27e6fc376ca8ea7
Christophe DANWE <christophe.danwe@korv-consulting.fr>
2024-05-07 11:41:48 +0100

    Updating Footer

--- components/partials/footer.js
+++ components/partials/footer.js
@@ -15,8 +15,8 @@ const Footer = () => {
           <div className="row gy-4">
             <div className={`col-lg-4 col-md-6 ${styles.footer_about}`}>
               <a href="index.html" className="d-flex align-items-center">
-                <span className={styles.sitename}> 
-                  <Image src="/logo-kei.png" width={80} height={80} alt="KEI Logo"/> 
+                <span className={styles.sitename}>
+                  <Image src="/logo-kei.png" width={80} height={80} alt="KEI Logo" />
                 </span>{" "}
               </a>{" "}
               <div className={`pt-3 ${styles.footer_contact}`}>
@@ -95,45 +95,71 @@ const Footer = () => {
                 {" "}
                 Visitez nos differents réseaux sociaux{" "}
               </p>{" "}
-              <div className={`${styles.social_links} d-flex`}>
+              {/* <div className={`${styles.social_links} d-flex`}>
                 <a href="">
                   {" "}
-                  <BsTwitterX/>
+                  <BsTwitterX />
                 </a>
                 <a href="https://www.facebook.com/korvconsulting/">
                   {" "}
-                  <BsFacebook/>
+                  <BsFacebook />
                 </a>
                 <a href="https://www.instagram.com/invites/contact/?i=1htitrb3x2j8j&utm_content=rxzmvja">
                   {" "}
-                  <BsInstagram/>
+                  <BsInstagram />
                 </a>
                 <a href="https://www.linkedin.com/feed/update/urn:li:activity:7086738201788248068">
                   {" "}
-                  <BsLinkedin/>
+                  <BsLinkedin />
                 </a>
-              </div>{" "}
+              </div>{" "} */}
             </div>
           </div>{" "}
         </div>
 
         <div
-          className={`container copyright ${styles.copyright} text-center mt-4`}
+          className={`container copyright ${styles.copyright}  mt-4`}
         >
-          <p>
-            {" "}
-            © <span> Copyright </span>{" "}
-            <strong className="px-1 sitename">KORV Estatement Inventory</strong>{" "}
-            <span> Tous droits réservés </span>
-          </p>
-          <div className="credits">
-            {" "}
-            {/* <!-- All the links in the footer should remain intact. -->
+          <div className="row">
+            <div className="col-lg-8 col-md-6">
+              <p>
+                {" "}
+                © <span> Copyright </span>{" "}
+                <strong className="px-1 sitename">KORV Estatement Inventory</strong>{" "}
+                <span> Tous droits réservés </span>
+              </p>
+              <div className="credits">
+                {" "}
+                {/* <!-- All the links in the footer should remain intact. -->
                                                                                                                                                                 <!-- You can delete the links only if you've purchased the pro version. -->
                                                                                                                                                                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
                                                                                                                                                                 <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] --> */}
-            {/* Designed by <a href="https://bootstrapmade.com/"> BootstrapMade </a>{" "} */}
-          </div>{" "}
+                {/* Designed by <a href="https://bootstrapmade.com/"> BootstrapMade </a>{" "} */}
+              </div>{" "}
+            </div>
+
+            <div className="col-lg-4 col-md-4">
+              <div className={`${styles.social_links} d-flex`}>
+                <a href="">
+                  {" "}
+                  <BsTwitterX />
+                </a>
+                <a href="https://www.facebook.com/korvconsulting/">
+                  {" "}
+                  <BsFacebook />
+                </a>
+                <a href="https://www.instagram.com/invites/contact/?i=1htitrb3x2j8j&utm_content=rxzmvja">
+                  {" "}
+                  <BsInstagram />
+                </a>
+                <a href="https://www.linkedin.com/feed/update/urn:li:activity:7086738201788248068">
+                  {" "}
+                  <BsLinkedin />
+                </a>
+              </div>{" "}
+            </div>
+          </div>
+
         </div>
       </footer>{" "}
     </div>
