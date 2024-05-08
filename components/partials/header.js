338f9f7078f3620ffe45c3a3a8c4db9a255deeb6
Christophe DANWE <christophe.danwe@korv-consulting.fr>
2024-05-08 10:57:22 +0100

    Adding Active Menu Item

--- components/partials/header.js
+++ components/partials/header.js
@@ -2,10 +2,15 @@ import styles from '@/styles/app.module.css'
 import { BsList } from 'react-icons/bs';
 import Image from 'next/image'
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Header() {
 
   const [isActive, setIsActive] = useState(1);
+
+  const router = useRouter();
+  console.log("Router +++++++++++++", router.asPath);
+
   return (
     <>
       <header id="header" className={`${styles.header} d-flex align-items-center fixed-top`}>
@@ -21,30 +26,13 @@ export default function Header() {
 
           <nav id="navmenu" className={`navmenu ${styles.navmenu}`}>
             <ul>
-              <li><a href="#home" onClick={() => {setIsActive(1); console.log("Active Menu Item", isActive)}} className={styles.active}>Accueil</a></li>
-              <li><a href="#about" onClick={() => setIsActive(2)}>A Propos</a></li>
-              <li><a href="#features" onClick={() => setIsActive(3)}>Fonctionnalités</a></li>
+              <li><a href="#home" onClick={() => {setIsActive(1); console.log("Active Menu Item", isActive)}} className={isActive == 1 ? styles.active : ""}>Accueil</a></li>
+              <li><a href="#about" onClick={() => setIsActive(2)} className={isActive == 2 ? styles.active : ""}>A Propos</a></li>
+              <li><a href="#features" onClick={() => setIsActive(3)} className={isActive == 3 ? styles.active : ""}>Fonctionnalités</a></li>
               {/* <li><a href="#portfolio">Portfolio</a></li> */}
               {/* <li><a href="#team">Team</a></li> */}
-              <li><a href="#pricing" onClick={() => setIsActive(4)}>Offres Tarifaires</a></li>
-              {/* <li className={`dropdown ${styles.dropdown}`}><a href="#"><span>Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown"></i></a>
-            <ul>
-              <li><a href="#">Dropdown 1</a></li>
-              <li className={`dropdown ${styles.dropdown}`}><a href="#"><span>Deep Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown"></i></a>
-                <ul>
-                  <li><a href="#">Deep Dropdown 1</a></li>
-                  <li><a href="#">Deep Dropdown 2</a></li>
-                  <li><a href="#">Deep Dropdown 3</a></li>
-                  <li><a href="#">Deep Dropdown 4</a></li>
-                  <li><a href="#">Deep Dropdown 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Dropdown 2</a></li>
-              <li><a href="#">Dropdown 3</a></li>
-              <li><a href="#">Dropdown 4</a></li>
-            </ul>
-          </li> */}
-              <li><a href="#contact" onClick={() => setIsActive(5)}>Contact</a></li>
+              <li><a href="#pricing" onClick={() => setIsActive(4)} className={isActive == 4 ? styles.active : ""}>Offres Tarifaires</a></li>
+              <li><a href="#contact" onClick={() => setIsActive(5)} className={isActive == 5 ? styles.active : ""}>Contact</a></li>
             </ul>
             <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
             <BsList className='mobile-nav-toggle d-xl-none'/>
