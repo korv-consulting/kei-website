400c18f4430b9ad9c6639e1edf047d7eece0b9ed
manuela1716 <manuelakengni10@gmail.com>
2024-06-11 18:36:30 +0200

    demo

--- components/partials/demo.js
+++ components/partials/demo.js
@@ -1,21 +1,83 @@
-import styles from '@/styles/app.module.css'
-import YouTube from "react-youtube";
-const Demo = () => {
+// import styles from '@/styles/app.module.css'
+// import YouTube from "react-youtube";
+// const Demo = () => {
 
+//   const opts = {
+//     height: "390",
+//     width: "640",
+//     playerVars: {
+//       autoplay: 1,
+//     },
+//   };
+
+//   const _onReady = (event) => {
+//     // Cette fonction est appelée lorsque le lecteur vidéo est prêt
+//     // Ici, nous mettons la vidéo en pause juste après le démarrage de l'autoplay
+//     event.target.pauseVideo();
+//   }
+
+
+//   return (
+//     <div>
+//       <section id="demo" className={`${styles.about} ${styles.section} `}>
+
+
+//         <div className={`container ${styles.section_title}`} data-aos="fade-up">
+//           <h2 className="">Démo</h2>
+//         </div>
+
+//         <div className="container">
+
+//           <div className="row gy-4">
+//             <div class="col-sm-12 col-lg-6 width-30 rounded">
+//               <YouTube videoId="xaJzA47EjQc" opts={opts} onReady={_onReady}  />
+//             </div>
+//             <div className="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+//               <p className='d-flex  text-justify' >
+//                 Rejoignez la communauté mondiale croissante des utilisateurs de KEI, qui comprend des individus et des équipes de certains des principaux acteurs de l'industrie.
+//                 Demandez des fonctionnalités sur mesure et organisez une démonstration de l'application KEI pour vous et votre entreprise.
+//               </p>
+//               <a className={`btn rounded-pill ${styles.btn_demo}`} href="/demo">
+//                 Obtenir une démo
+//               </a>
+
+//               {/* <a href="#" className={styles.btn_getstarted}><span>Obtenir une démo</span><i className="bi bi-arrow-right"></i></a> */}
+//             </div>
+
+//           </div>
+
+//         </div>
+
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default Demo;
+
+
+// components/Demo/Demo.js
+import React from 'react';
+import YouTubePlayer from 'react-youtube';
+import styles from '@/styles/Demo.module.css';
+import style from '@/styles/app.module.css';
+
+
+const Demo = () => {
   const opts = {
-    height: "390",
-    width: "640",
+    height: '200',
+    width: '100%',
     playerVars: {
       autoplay: 1,
+      modestbranding: 1,
+      controls: 1,
+      rel: 0,
     },
   };
 
   const _onReady = (event) => {
-    // Cette fonction est appelée lorsque le lecteur vidéo est prêt
-    // Ici, nous mettons la vidéo en pause juste après le démarrage de l'autoplay
     event.target.pauseVideo();
-  }
-
+  };
 
   return (
     <div>
@@ -46,12 +108,10 @@ const Demo = () => {
             </div>
 
           </div>
-
         </div>
-
-      </section>
-    </div>
+      </div>
+    </section>
   );
-}
+};
 
 export default Demo;
